45c319991bb1adf3bc5275b9b57f6931b8faa2b0
JeffRisberg <jrisberg@aol.com>
2015-11-26 09:07:30 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import BootState from './states/BootState';
+import LoadingState from './states/LoadingState';
+import PlayState from './states/PlayState';
+
+class FruitNinja extends Phaser.Game {
+
+    constructor() {
+        super("100%", "100%", Phaser.CANVAS, 'content', null);
+
+        this.state.add('boot', BootState, false);
+        this.state.add('loading', LoadingState, false);
+        this.state.add('play', PlayState, false);
+
+        this.state.start('boot');
+    }
+}
+
+new FruitNinja();
---
+++ src/states/BootState.js
@@ -0,0 +1,23 @@
+import Container from '../prefabs/Container';
+
+class BootState extends Phaser.State {
+
+    constructor() {
+        super();
+    }
+
+    init(level_file) {
+        this.level_file = level_file;
+    }
+
+    preload() {
+        this.load.text("level1", this.level_file);
+    }
+
+    create() {
+        var level_text = this.game.cache.getText("level1");
+        var level_data = JSON.parse(level_text);
+
+        this.game.state.start("LoadingState", true, false, level_data);
+    }
+}
